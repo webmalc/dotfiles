48045485d6c8be5b8960e4bc6c5d16bcb1291001
webmalc <m@webmalc.pw>
2018-02-19 16:43:49 +0300

    i3status config

--- compile.py
+++ compile.py
@@ -8,16 +8,27 @@ def get_config():
     return config
 
 
-def mutt(config):
-    # compile mutt
-    print('compile mutt')
-    with open('mutt/muttrc.template', 'r') as template:
+def _compile_config(config, path):
+    # compile
+    print('compile' + path)
+    with open(path + '.template', 'r') as template:
         content = template.read()
-        with open('mutt/muttrc.compiled', 'w+') as compiled:
+        with open(path + '.compiled', 'w+') as compiled:
             compiled_content = content.replace('{{email_password}}',
                                                config['email']['password'])
             compiled.write(compiled_content)
 
 
+def mutt(config):
+    # compile mutt
+    _compile_config(config, 'mutt/muttrc')
+
+
+def i3status(config):
+    # compile i3status
+    _compile_config(config, 'i3/i3status.conf')
+
+
 config = get_config()
 mutt(config)
+i3status(config)
