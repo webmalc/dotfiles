b9e89a0298ee72dea5f8f740159808269858ae7b
webmalc <m@webmalc.pw>
2019-09-01 14:36:13 +0300

    Update the conky calendar script

---
+++ conky/cal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import re
+from calendar import month
+from datetime import date
+
+today = date.today()
+day = str(date.today().day)
+result = month(today.year, today.month)
+result = re.sub(day + '(?!\d)',
+                '${color1}' + day + '${color}',
+                result,
+                count=1)
+print(result)
