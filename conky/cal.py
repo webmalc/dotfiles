da137c71e83631105fb187e24d3c407e47592556
webmalc <m@webmalc.pw>
2019-09-02 13:58:07 +0300

    Add updates for the conky calendar

--- conky/cal.py
+++ conky/cal.py
@@ -6,8 +6,8 @@ from datetime import date
 today = date.today()
 day = str(date.today().day)
 result = month(today.year, today.month)
-result = re.sub(day + '(?!\d)',
-                '${color1}' + day + '${color}',
-                result,
-                count=1)
+result = re.sub(
+    day + '(?!\d)', '${color1}' + day + '${color}', result, count=1)
+result = result.replace('Sa', '${color2}Sa${color}')
+result = result.replace('Su', '${color2}Sa${color}')
 print(result[:-1])
