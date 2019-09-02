e40dacabeb8050b871965f8d39f58f9f6e24a99e
webmalc <m@webmalc.pw>
2019-09-02 16:44:29 +0300

    Update the conky calendar

--- conky/cal.py
+++ conky/cal.py
@@ -6,8 +6,21 @@ from datetime import date
 today = date.today()
 day = str(date.today().day)
 result = month(today.year, today.month)
+weekends = []
+days = iter(result.splitlines())
+days.next()
+for line in days:
+    elements = re.split('\s', line)
+    if len(elements) >= 7:
+        elements = filter(lambda x: x != '', elements)
+        weekends.extend([e for e in elements[-2:] if e != day])
+
 result = re.sub(
     day + '(?!\d)', '${color1}' + day + '${color}', result, count=1)
-result = result.replace('Sa', '${color2}Sa${color}')
-result = result.replace('Su', '${color2}Sa${color}')
+for weekend in weekends:
+    result = re.sub(
+        weekend + '(?!\d)',
+        '${color2}' + weekend + '${color}',
+        result,
+        count=1)
 print(result[:-1])
