7e6a9b29d7b62916a7257c5ff4d97a54378c9c8a
webmalc <m@webmalc.pw>
2019-09-04 21:23:29 +0300

    Update the vocab conky script

--- conky/vocab.py
+++ conky/vocab.py
@@ -1,15 +1,49 @@
 #!/usr/bin/python
+import os
+import random
 import textwrap
 import urllib2
 
 from bs4 import BeautifulSoup
 
-html_page = urllib2.urlopen('https://www.vocabulary.com/dictionary/randomword')
-soup = BeautifulSoup(html_page, 'html.parser')
-word = soup.select_one('h1.dynamictext').text
-word = '${font Ubuntu Mono:size=16}' + word.capitalize()
-text = soup.select_one('p.short').text
-text = '${font Ubuntu Mono:size=12}' + textwrap.fill(text, 30)
-result = u'{}\n\n{}'.format(word, text)
 
-print(result.encode('utf-8'))
+def random_line(afile):
+    line = next(afile)
+    for num, aline in enumerate(afile, 2):
+        if random.randrange(num):
+            continue
+        line = aline
+    return line
+
+
+def get_word(random_word):
+    html_page = urllib2.urlopen('https://www.vocabulary.com/dictionary/' +
+                                random_word)
+    soup = BeautifulSoup(html_page, 'html.parser')
+    word = soup.select_one('h1.dynamictext').text
+    word = '${font Ubuntu Mono:size=16}' + word.capitalize()
+
+    element = soup.select_one('p.short')
+    if element:
+        text = element.text
+    else:
+        text = soup.select_one('h3.definition').text[10:].strip()
+    text = '${font Ubuntu Mono:size=12}' + textwrap.fill(text, 30)
+    result = u'{}\n\n{}'.format(word, text)
+    return result
+
+
+def get_random_word():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(base_dir, 'words.txt')
+    with open(filename, 'r') as words_file:
+        random_word = random_line(words_file).strip()
+    return random_word
+
+
+try:
+    CONTENT = get_word(get_random_word())
+except:
+    CONTENT = get_word('randomword')
+
+print(CONTENT.encode('utf-8'))
