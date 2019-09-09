f8234f7a70bda702dd013e6ab300117f22969d82
webmalc <m@webmalc.pw>
2019-09-09 12:14:09 +0300

    Update the calendar script

--- conky/cal.py
+++ conky/cal.py
@@ -7,20 +7,24 @@ today = date.today()
 day = str(date.today().day)
 result = month(today.year, today.month)
 weekends = []
-days = iter(result.splitlines())
-days.next()
-for line in days:
+days = result.splitlines()
+header = days[0]
+content = '\n'.join(days[1:])
+
+for line in days[1:]:
     elements = re.split('\s', line)
     if len(elements) >= 7:
         elements = filter(lambda x: x != '', elements)
         weekends.extend([e for e in elements[-2:] if e != day])
 
-result = re.sub(
-    day + '(?!\d)', '${color1}' + day + '${color}', result, count=1)
+content = re.sub(day + '(?!\d)',
+                 '${color1}' + day + '${color}',
+                 content,
+                 count=1)
 for weekend in weekends:
-    result = re.sub(
-        weekend + '(?!\d)',
-        '${color2}' + weekend + '${color}',
-        result,
-        count=1)
+    content = re.sub(weekend + '(?!\d)',
+                     '${color2}' + weekend + '${color}',
+                     content,
+                     count=1)
+result = header + '\n' + content
 print(result[:-1])
