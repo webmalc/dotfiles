28501843182b087ea5ffd24c332f7d9b237b55da
webmalc <m@webmalc.pw>
2019-11-06 15:48:32 +0300

    Update the calendar script

--- conky/cal.py
+++ conky/cal.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python
 import re
-from calendar import month
+from calendar import SUNDAY, TextCalendar, month
 from datetime import date
 
 today = date.today()
 day = str(date.today().day)
 result = month(today.year, today.month)
-weekends = []
+weekends = ['Sa', 'Su']
 days = result.splitlines()
 header = days[0]
 content = '\n'.join(days[1:])
 
-for line in days[1:]:
-    elements = re.split('\s', line)
-    if len(elements) >= 7:
-        elements = filter(lambda x: x != '', elements)
-        weekends.extend([e for e in elements[-2:] if e != day])
+cal = TextCalendar(SUNDAY)
+for i in cal.itermonthdays(today.year, today.month):
+    if i != 0:
+        if date(today.year, today.month, i).weekday() >= 5:
+            weekends.append(str(i))
 
-content = re.sub(day + '(?!\d)',
-                 '${color1}' + day + '${color}',
-                 content,
-                 count=1)
+content = re.sub(
+    day + '(?!\d)', '${color1}' + day + '${color}', content, count=1)
 for weekend in weekends:
-    content = re.sub(weekend + '(?!\d)',
-                     '${color2}' + weekend + '${color}',
-                     content,
-                     count=1)
+    content = re.sub(
+        weekend + '(?![\d,\}])',
+        '${color2}' + weekend + '${color}',
+        content,
+        count=1)
 result = header + '\n' + content
-print(result[:-1])
+print(result)
