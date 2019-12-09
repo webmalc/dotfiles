aa18dc7ad32db0493cc9b9e22d379a886a5fb50a
webmalc <m@webmalc.pw>
2019-12-09 21:59:14 +0300

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 #eval (python -m virtualfish)
 thefuck --alias | source
 set EDITOR vim; export EDITOR
+set PYTHONBREAKPOINT 'ipdb.set_trace'; export PYTHONBREAKPOINT
 python -m virtualfish | source
 
