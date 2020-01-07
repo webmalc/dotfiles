bfda2ae1b60d30671e85bf98cb805f83c7c05fac
webmalc <m@webmalc.pw>
2020-01-07 13:02:49 +0300

    Update the emacs rofi script

--- rofi/scripts/emacs.sh
+++ rofi/scripts/emacs.sh
@@ -9,7 +9,17 @@ stop="stop"
 # Variable passed to rofi
 options="$start\n$stop\n$reboot"
 
-chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0 -p emacs)"
+
+active="$(systemctl --user status emacs | grep running)"
+if [ -n "$active" ]; then
+    status="emacs"
+    status_style="#prompt { background-color: @on; }"
+else
+    status="emacs"
+    status_style="#prompt { background-color: @off; }"
+fi
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0 -theme-str "$status_style" -p "$status")"
+
 case $chosen in
     $start)
         systemctl --user start emacs; bash -c " wmctrl -xa emacs.Emacs || emacsclient -nc -s instance1"
