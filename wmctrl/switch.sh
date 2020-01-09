823f3e9852f195e02453f66a154f3564e3b64431
webmalc <m@webmalc.pw>
2020-01-10 00:32:48 +0300

    Add the mpd

--- wmctrl/switch.sh
+++ wmctrl/switch.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
+function get_proper_title() {
+    # echo $1 | awk '{print $5" "$6" "$7}' | \
+    #     sed -e 's/N\/A//' | \
+    #     sed -e 's/[[:space:]]*$//' | sed -e 's/^[[:space:]]//'
+    echo $1 | awk '{print $5}'
+}
 WID=`xprop -root | grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}'`
 WID="${WID:2}"
 ACTIVE_WIN=`wmctrl -lx | grep $WID`
 ACTIVE_WIN_CLASS=`echo $ACTIVE_WIN | awk '{print $3}'`
+ACTIVE_WIN_ID=`echo $ACTIVE_WIN | awk '{print $1}'`
+ACTIVE_WIN_TITLE=`get_proper_title "$ACTIVE_WIN"`
 DESTINATION_WIN_CLASS=$1
 DESTINATION_APP=$2
 HISTORY_FILE=/tmp/windows_history.log
+LOG_FILE=/tmp/windows_log.log
+PARAMS=a
+
+# echo "_________________________________________" >> $LOG_FILE
+
+if [[ $DESTINATION_WIN_CLASS == *"."* ]]; then
+    PARAMS=xa
+else
+    ACTIVE_WIN_CLASS=$ACTIVE_WIN_TITLE
+fi
+
+# echo "$ACTIVE_WIN_CLASS - $DESTINATION_WIN_CLASS" >> $LOG_FILE
 
 if [[ $ACTIVE_WIN_CLASS == $DESTINATION_WIN_CLASS ]]; then
-    PREV_WIN_CLASS=$(tail -1 $HISTORY_FILE)
-    PREV_PREV_WIN_CLASS=$(tail -2 $HISTORY_FILE | head -1)
 
-    if [[ $PREV_WIN_CLASS != $ACTIVE_WIN_CLASS ]]; then
-        wmctrl -xa  "$PREV_WIN_CLASS"
-        echo $PREV_PREV_WIN_CLASS >> $HISTORY_FILE
-    elif [[ $PREV_PREV_WIN_CLASS != $ACTIVE_WIN_CLASS ]]; then
-        wmctrl -xa  "$PREV_PREV_WIN_CLASS"
-        echo $ACTIVE_WIN_CLASS >> $HISTORY_FILE
+    PREV_WIN_ID=$(tail -1 $HISTORY_FILE)
+    PREV_PREV_WIN_ID=$(tail -2 $HISTORY_FILE | head -1)
+
+    PREV_WIN=`wmctrl -lx | grep $PREV_WIN_ID`
+    PREV_WIN_TITLE=`get_proper_title "$PREV_WIN"`
+
+    PREV_PREV_WIN=`wmctrl -lx | grep $PREV_PREV_WIN_ID`
+    PREV_PREV_WIN_TITLE=`get_proper_title "$PREV_PREV_WIN"`
+
+
+    # echo "---- $PREV_WIN_TITLE" >> $LOG_FILE
+
+    if [[ $PREV_WIN_TITLE != $ACTIVE_WIN_TITLE ]]; then
+        wmctrl -a  "$PREV_WIN_TITLE"
+        echo $ACTIVE_WIN_ID >> $HISTORY_FILE
+    elif [[ $PREV_PREV_WIN_TITLE != $ACTIVE_WIN_TITLE ]]; then
+        wmctrl -$PARAMS  "$PREV_PREV_WIN_TITLE"
+        echo $ACTIVE_WIN_ID >> $HISTORY_FILE
     fi
 else
     touch $HISTORY_FILE
-    wmctrl -xa "$DESTINATION_WIN_CLASS" || $DESTINATION_APP & 
-    echo $ACTIVE_WIN_CLASS >> $HISTORY_FILE
+    wmctrl -$PARAMS "$DESTINATION_WIN_CLASS" || $DESTINATION_APP & 
+    echo $ACTIVE_WIN_ID >> $HISTORY_FILE
 fi
 
 
