e19762229c002c7546b6902760552d9ea20d296d
webmalc <m@webmalc.pw>
2020-02-10 11:11:25 +0300

    Updated the mpv script

--- rofi/scripts/mpv.sh
+++ rofi/scripts/mpv.sh
@@ -9,14 +9,14 @@ stop="stop"
 options="$start\n$stop\n$reboot"
 
 
-active="$(pgrep mpv)"
-if [ -n "$active" ]; then
-    status="mpv"
-    status_style="#prompt { background-color: @on; }"
-else
-    status="mpv"
-    status_style="#prompt { background-color: @off; }"
-fi
+if pgrep -x "mpv" > /dev/null
+   then
+       status="mpv"
+       status_style="#prompt { background-color: @on; }"
+   else
+       status="mpv"
+       status_style="#prompt { background-color: @off; }"
+   fi
 chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 0 -theme-str "$status_style" -p "$status")"
 
 case $chosen in
