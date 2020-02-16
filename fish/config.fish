1321a95c79266043d6cbf2dd28d8c7688c04e33b
webmalc <m@webmalc.pw>
2020-02-16 17:45:10 +0300

    Update the dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ set EDITOR vim; export EDITOR
 set PYTHONBREAKPOINT 'ipdb.set_trace'; export PYTHONBREAKPOINT
 set -x -U GOPATH $HOME/go
 set -x -U BROWSER firefox
+set -x -U CHROME_BIN chromium-browser
 set -gx PATH $GOPATH/bin $PATH
 python -m virtualfish | source
 
