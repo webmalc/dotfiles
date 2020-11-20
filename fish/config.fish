846da0710056d94d2e3590a104fb0069e3039e62
webmalc <m@webmalc.pw>
2020-11-20 20:43:54 +0300

    Update the shell config

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,6 @@ set -x -U GOPATH $HOME/go
 set -x -U BROWSER firefox
 set -x -U CHROME_BIN google-chrome
 set -gx PATH $GOPATH/bin $PATH
-set -g simple_ass_prompt_greeting (set_color blue)(date) (set_color white) - (watson status)
+set -g simple_ass_prompt_greeting
+set --erase fish_greeting
 thefuck --alias | source
